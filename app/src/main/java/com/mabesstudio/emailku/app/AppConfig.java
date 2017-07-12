16a4c6185567a3142aa24f1703d10f560786c960
edof <edoucreedthink@gmail.com>
2017-07-12 16:29:32 +0700

    Initial commit

---
+++ app/src/main/java/com/mabesstudio/emailku/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.mabesstudio.emailku.app;
+
+/**
+ * Created by Misbahul on 26/06/2017.
+ */
+
+public class AppConfig {
+    public static String URL_EMAIL_DATA = "http://api.androidhive.info/json/inbox.json";
+}
