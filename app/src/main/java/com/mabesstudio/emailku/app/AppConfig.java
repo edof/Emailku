f4232b6423768b5c871962e6afafb13dec0f7c82
Misbahul <m3.rezpect@gmail.com>
2017-07-19 19:32:26 +0700

    set url data dan key JSON

--- app/src/main/java/com/mabesstudio/emailku/app/AppConfig.java
+++ app/src/main/java/com/mabesstudio/emailku/app/AppConfig.java
@@ -1,9 +1,11 @@
 package com.mabesstudio.emailku.app;
 
-/**
- * Created by Misbahul on 26/06/2017.
- */
-
 public class AppConfig {
-    public static String URL_EMAIL_DATA = "http://api.androidhive.info/json/inbox.json";
+    public static String URL_EMAIL_DATA = "http://www.json-generator.com/api/json/get/cgvXoeVQia?indent=2";
+    public static String KEY_SENDER = "sender";
+    public static String KEY_SUBJECT = "subject";
+    public static String KEY_SNIPPET = "snippet";
+    public static String KEY_STAR = "star";
+    public static String KEY_TIMESTAMP = "timestammp";
+    public static String KEY_ATTACHMENT = "attachment";
 }
