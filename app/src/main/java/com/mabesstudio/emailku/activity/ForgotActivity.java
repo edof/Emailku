882a5b23cafd06de84f0b8d4ee344e93fe7e0cec
edof <edoucreedthink@gmail.com>
2017-07-24 09:21:14 +0700

    splash screen, mipmap icon

--- app/src/main/java/com/mabesstudio/emailku/activity/ForgotActivity.java
+++ app/src/main/java/com/mabesstudio/emailku/activity/ForgotActivity.java
@@ -3,8 +3,10 @@ package com.mabesstudio.emailku.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +22,15 @@ public class ForgotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_forgot);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Forgot Password");
+        }
+
+
 
         editForgot = (EditText) findViewById(R.id.edit_forgot_email);
         btnForgot = (Button) findViewById(R.id.btn_forgot);
@@ -41,5 +52,16 @@ public class ForgotActivity extends AppCompatActivity {
 
     public final static boolean isValidEmail (CharSequence sequence){
         return !TextUtils.isEmpty(sequence) && Patterns.EMAIL_ADDRESS.matcher(sequence).matches();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+        }
     }
 }
