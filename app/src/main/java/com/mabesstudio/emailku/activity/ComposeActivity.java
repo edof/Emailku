576827337f8d6e2d7745b21b7b49b6925d5000ba
edof <edoucreedthink@gmail.com>
2017-07-27 18:52:08 +0700

    add compose activity

---
+++ app/src/main/java/com/mabesstudio/emailku/activity/ComposeActivity.java
@@ -0,0 +1,42 @@
+package com.mabesstudio.emailku.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mabesstudio.emailku.R;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_compose);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("Tulis Email");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.compose, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
