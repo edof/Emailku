c0aa309c23e56a03fd8d0ec84eb2064f4af54863
edof <edoucreedthink@gmail.com>
2017-07-30 20:15:28 +0700

    add reply activity, attachment layout, and need more fixes with all layouts

--- app/src/main/java/com/mabesstudio/emailku/activity/ReplyActivity.java
+++ app/src/main/java/com/mabesstudio/emailku/activity/ReplyActivity.java
@@ -2,6 +2,9 @@ package com.mabesstudio.emailku.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.mabesstudio.emailku.R;
 
@@ -11,5 +14,29 @@ public class ReplyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reply);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_reply);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("Balas");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.compose, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+        }
     }
 }
